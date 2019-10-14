46465d191194624e2e3f3dda1115feb523cf6881
Jigong82 <56555425+Jigong82@users.noreply.github.com>
2019-10-14 23:45:37 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+// configurasi //
+
+
+// user_name boleh kosong //
+$user_name="Aricug";
+
+//user_google_ad_id wajib isi
+$user_google_ad_id="2feadbdc-d39d-4c78-9a71-7ec0f7a1bf5b";
+//user_token wajib di isi //
+$user_token="AbzbS9QT43bURCbS";
+
+//user_id wajib di isi //
+$user_id="Facebook_137020961026276";
+
+// invite_code wajib di isi //
+$invite_code="FcuKVof9o";
+
+// Bearer wajib di isi //
+$Bearer="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiJGYWNlYm9va18xMzcwMjA5NjEwMjYyNzYiLCJleHAiOiIxNTczMzkzMjAzMjE0In0.gZUBlvbtBgWwqQjt-74W1uHNNsqN0Mhe0hysH0ppoNs";
+
+
+
+?>
